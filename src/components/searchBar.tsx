1ea1f5e4e85b71e5fa22dda16d73081a90ab7317
wldnjs990 <wl990@naver.com>
2025-05-15 11:14:25 +0900

    feat : searchbar 컴포넌트 생성

---
+++ src/components/searchBar.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Shadow from "./shadow";
+
+export default function SearchBar() {
+  return (
+    <div className="relative flex-1">
+      <input
+        placeholder="할 일을 입력해주세요"
+        className="w-full h-full px-6 py-4.25 rounded-3xl border-2 border-slate-900 placeholder:text-slate-500 bg-white grow"
+      />
+      <Shadow />
+    </div>
+  );
+}
