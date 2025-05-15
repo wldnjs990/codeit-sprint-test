dd1386d51a87516dc87ed619735949c662fdf3b9
wldnjs990 <wl990@naver.com>
2025-05-15 11:15:30 +0900

    design : 헤더 fixed position 부여

--- src/components/navigate.tsx
+++ src/components/navigate.tsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 export default function Navigate() {
   return (
-    <header className="border-b border-b-slate-200 bg-white">
-      <nav className="flex max-w-[1200px] w-full mx-auto px-6 py-2.5 ">
+    <header className="border-b border-b-slate-200 w-full bg-white fixed top-0 left-0 z-40">
+      <nav className="flex max-w-[1200px] w-full mx-auto px-6 py-2.5">
         <Link href={"/"}>
           <img src="/logo.svg" alt="로고" />
         </Link>
