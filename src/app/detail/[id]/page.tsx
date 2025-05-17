b5a9f7950e84f4ca3967e97e0f959717b8e7f5bd
wldnjs990 <wl990@naver.com>
2025-05-17 17:28:38 +0900

    refactor : useEffect훅 실행 함수 useCallback으로 캐싱후 의존성 배열 추가

--- src/app/detail/[id]/page.tsx
+++ src/app/detail/[id]/page.tsx
@@ -5,7 +5,7 @@ import DetailTitle from "./components/DetailTitle";
 import DetailContent from "./components/DetailContent";
 import { useParams } from "next/navigation";
 import { getTodoOne } from "@/lib/get-todo-one";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import DetailButtons from "./components/DetailButtons";
 
 export default function Detail() {
@@ -23,13 +23,13 @@ export default function Detail() {
   });
 
   // 클라이언트로 todo디테일 데이터 가져오기(싹 다 클라이언트 컴포넌트 사용하면 이게 리액트랑 뭐가 다르지 흠)
-  const getTodoDetail = async () => {
+  const getTodoDetail = useCallback(async () => {
     const todo = await getTodoOne(id);
     if (todo) setTodo(todo);
-  };
+  }, [id]);
   useEffect(() => {
     getTodoDetail();
-  }, []);
+  }, [getTodoDetail]);
 
   // 페이지 자체가 클라이언트 렌더링이라 받아오는 과정에 로딩 fallback을 넣었습니다.
   if (todo.id === 0)
