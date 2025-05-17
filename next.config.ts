77be960ecb89d29f878778fd588dc5f90b2d2e81
wldnjs990 <wl990@naver.com>
2025-05-17 17:27:43 +0900

    chore : images 경로 허용 도메인 설정

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: false,
+  images: {
+    domains: ["sprint-fe-project.s3.ap-northeast-2.amazonaws.com"],
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
